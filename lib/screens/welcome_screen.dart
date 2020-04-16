4642f31fe3e882641469a9cdeaa6f39960de0d8c
luiscarragar1 <lgalviz@uninorte.edu.co>
2020-04-16 03:20:33 -0500

    Animation Logo

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -8,7 +8,30 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    animation=CurvedAnimation(parent: controller, curve: Curves.bounceOut);
+
+    controller.forward();
+    controller.addListener((){
+      setState(() {
+        
+      });
+      print(controller.value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +44,12 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: animation.value *100,
+                  ),
                 ),
                 Text(
                   'Flash Chat',
